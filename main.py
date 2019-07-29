75eb1d5b1f352bf3aad7cd6f86d80fbe9d7b1a37
A D <moeknows@gmail.com>
2019-07-29 16:08:39 -0700

    Adding activity from lesson06

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import requests
 # (or another user name)
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Enter a github username")
+        print("Syntax: {} <github_username>".format(sys.argv[0]))
+        quit(1)
+
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
 
-    print("COMPLETE THE TODOs")
+    print(events[0]['created_at'])
     
 
 
